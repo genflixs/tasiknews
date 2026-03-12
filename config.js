9e7cfcd63d3c2b8f20c47cccbb30824dc008f07b
muniermorgan27 <54472437+muniermorgan27@users.noreply.github.com>
2026-03-12 23:55:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+
+SITE_NAME: "Tasik News",
+
+SITE_URL: "https://genflixs.github.io/tasiknews",
+
+NEWS_API_KEY: "9f8af9f4b277493c842c02e16441214f",
+
+NEWS_API_URL:
+"https://newsapi.org/v2/top-headlines?country=id&pageSize=30&apiKey=",
+
+IMAGE_RANDOM:
+"https://picsum.photos/900/500?random="
+
+}
---
+++ generator/generate-news.js
@@ -0,0 +1,108 @@
+const fs = require("fs")
+const fetch = require("node-fetch")
+
+const APIKEY = "9f8af9f4b277493c842c02e16441214f"
+
+const API =
+`https://newsapi.org/v2/top-headlines?country=id&pageSize=30&apiKey=${APIKEY}`
+
+const IMAGE =
+"https://picsum.photos/900/500?random="
+
+async function generate(){
+
+const res = await fetch(API)
+
+const data = await res.json()
+
+let posts = []
+
+for(let i=0;i<data.articles.length;i++){
+
+let a=data.articles[i]
+
+let slug=a.title
+.toLowerCase()
+.replace(/[^a-z0-9]/g,"-")
+.slice(0,80)
+
+let image=IMAGE+i
+
+let article =
+
+`Berita terbaru mengenai ${a.title}.
+
+Informasi ini disusun kembali agar lebih mudah dipahami pembaca.
+
+${a.description}
+
+Perkembangan informasi masih terus berlangsung sehingga masyarakat disarankan mengikuti update terbaru dari media terpercaya.`
+
+let html =
+
+`
+<html>
+
+<head>
+
+<title>${a.title} | Tasik News</title>
+
+<meta name="description" content="${a.description}">
+
+<script type="application/ld+json">
+{
+"@context":"https://schema.org",
+"@type":"NewsArticle",
+"headline":"${a.title}",
+"publisher":{
+"name":"Tasik News"
+}
+}
+</script>
+
+</head>
+
+<body>
+
+<h1>${a.title}</h1>
+
+<img src="${image}">
+
+<p>${article}</p>
+
+<p>
+Sumber:
+<a href="${a.url}">
+${a.source.name}
+</a>
+</p>
+
+</body>
+
+</html>
+`
+
+fs.writeFileSync(`articles/${slug}.html`,html)
+
+posts.push({
+
+title:a.title,
+
+slug:slug,
+
+image:image,
+
+desc:a.description
+
+})
+
+}
+
+fs.writeFileSync(
+"articles/index.json",
+JSON.stringify(posts,null,2)
+)
+
+}
+
+generate()
